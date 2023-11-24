826389a67114320d348bb5db431d5a6168924c50
magnus aune <mau019@uib.no>
2023-11-24 14:11:11 +0100

    login3.0

--- src/main/java/VotingApp/login/LoginDAO.java
+++ src/main/java/VotingApp/login/LoginDAO.java
@@ -2,17 +2,17 @@ package VotingApp.login;
 import VotingApp.security.Hasher;
 import VotingApp.user.User;
 import VotingApp.user.UserDAO;
-import jakarta.persistence.*;
-import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
-import org.jose4j.jwe.JsonWebEncryption;
-import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
-import org.jose4j.keys.AesKey;
-import org.jose4j.lang.ByteUtil;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.List;
+
+import static io.jsonwebtoken.Jwts.SIG.HS256;
 
 
 @Service
@@ -23,26 +23,33 @@ public class LoginDAO {
 
     public String verifyUser(String name, String password) throws Exception {
         String hashed = Hasher.toHexString(Hasher.getSHA(password));
-        System.out.println("Name: " + name + " Password: " + password+ " Hashed: " + hashed);
         List<User> users = userDAO.getUsersByName(name);
         User theOne = null;
         for (User user : users) {
             if (hashed.equals(user.getPassword())) theOne = user;
         }
+        MacAlgorithm alg = Jwts.SIG.HS512; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+        byte[] content = userToJson(theOne).getBytes(StandardCharsets.UTF_8);
 
-        Key key = new AesKey(ByteUtil.randomBytes(16));
-        JsonWebEncryption jwe = new JsonWebEncryption();
-        jwe.setPayload(theOne.toString());
-        jwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.A128KW);
-        jwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
-        jwe.setKey(key);
-        String serializedJwe = jwe.getCompactSerialization();
+// Create the compact JWS:
+        String jws = Jwts.builder().content(content, "text/plain").signWith(key, alg).compact();
+
+        System.out.println("Serialized Encrypted JWE: " + jws);
         if (theOne != null) {
-            return serializedJwe;
+            return jws;
         } else {
             return null;
         }
     }
-
+    private String userToJson(User user) {
+        return "{" +
+                "\"userId\":" + user.getUserID() + "," +
+                "\"name\":\"" + user.getName() + "\"," +
+                "\"email\":\"" + user.getEmail() + "\"," +
+                "\"password\":\"" + user.getPassword() + "\"," +
+                "\"isAdmin\":" + user.getIsAdmin() +
+                "}";
+    }
 
 }
